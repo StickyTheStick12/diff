7e6231c7cbfcbfbf1093c72d0c5bc6643a8f7c31
StickyTheStick12 <89857252+StickyTheStick12@users.noreply.github.com>
2023-02-13 16:19:56 +0100

    Update SPN.py

--- SPN.py
+++ SPN.py
@@ -1,8 +1,11 @@
 import binascii
 import os
 
+# TODO:
+# keygen seed might need to be bigger check
+
 sbox = {0: 0xE, 1: 0x4, 2: 0xD, 3: 0x1, 4: 0x2, 5: 0xF, 6: 0xB, 7: 0x8, 8: 0x3, 9: 0xA, 0xA: 0x6, 0xB: 0xC,
-        0xD: 0x9, 0xE: 0x0, 0xF: 0x7}
+        0xC: 0x5, 0xD: 0x9, 0xE: 0x0, 0xF: 0x7}
 
 sboxInverse = {0xE: 0, 0x4: 1, 0xD: 2, 0x1: 3, 0x2: 4, 0xF: 5, 0xB: 6, 0x8: 7, 0x3: 8, 0xA: 9, 0x6: 0xA, 0xC: 0xB,
                0x5: 0xC, 0x9: 0xD, 0x0: 0xE, 0x7: 0xF}
@@ -10,73 +13,77 @@ sboxInverse = {0xE: 0, 0x4: 1, 0xD: 2, 0x1: 3, 0x2: 4, 0xF: 5, 0xB: 6, 0x8: 7, 0
 perm = [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15]
 
 
-def keygen(p=False) -> str:
-    key = str(binascii.b2a_hex(os.urandom(10)))
+def keygen(p=False) -> bytes:
+    key = binascii.b2a_hex(os.urandom(10))
 
     if p:
         for i in range(5):
-            print(f"key{i} = {key[i*4:i*4+4]}")
-
+            print(f"key{i} = {str(key)[i*4:i*4+4]}")
     return key
 
 
-def appSbox(bits: str, p=False) -> int:
-    s = ""
+def appSbox(bits: int, p=False) -> int:
+    bitParts = [(bits & (0x000f << i)) >> i for i in range(0, 16, 4)]
 
-    for i in range(4):
-        s += sbox[int(bits[i*4: i*4+4], 16)]
+    for index, value in enumerate(bitParts):
+        bitParts[index] = sbox[value]
+
+    value = 0
+    for i in range(0, 16, 4):
+        value += (bitParts[i//4] << i)
 
     if p:
-        print(s)
+        print(value)
 
-    return int(s, 16)
-
-
-def appPerm(bits: str) -> str:
-    newBits = [None] * 16
-
-    for i in range(16):
-        newBits[perm[i]] = bits[i]
-
-    return int("".join(newBits), 16)
+    return value
 
 
-def encrypt(text: str, key: str, verbose=False) -> int:
+def appPerm(bits: int) -> int:
+    temp = 0
+
+    for index in range(16):
+        if bits & (1 << index) != 0:
+            temp += 1 << perm[index]
+
+    return temp
+
+
+def encrypt(text: int, key: bytes, verbose=False) -> int:
     if verbose:
         temp = text
-        print(f"encrypting this: {int(text, 16)}")
+        print(f"encrypting this: {text}")
         subKeys = [int(key[i*4:i*4+4], 16) for i in range(5)]
 
         for round in range(0, 3):
             if verbose:
-                print(f"roundnumber {round}", end=" ")
+                print(f"round {round}", end="")
 
-            temp = temp ^ subKeys[round]
-
-            if verbose:
-                print(f", {temp}", end=" ")
-
-            temp = int(appSbox(str(temp)), 16)
+            temp = temp^subKeys[round]
 
             if verbose:
-                print(f", {temp}", end=" ")
+                print(f", {hex(temp)}", end="")
 
-            appPerm(str(temp))
+            temp = appSbox(temp)
 
             if verbose:
-                print(f", {temp}")
+                print(f", {hex(temp)}", end="")
 
-        temp = int(temp, 16) ^ subKeys[-2]
+            temp = appPerm(temp)
+
+            if verbose:
+                print(f", {hex(temp)}")
+
+        temp = temp ^ subKeys[-2]
 
         if verbose:
-            print(f"roundnumber 3 {temp}", end=" ")
+            print(f"round 3 {hex(temp)}")
 
-        temp = appSbox(str(temp))
+        temp = appSbox(temp)
 
         if verbose:
-            print(f", {temp}")
+            print(f"round 4 {hex(temp)}")
 
-        temp = int(temp, 16) ^ subKeys[-1]
-        print(temp)
+        temp = temp^subKeys[-1]
+        print(f"encrypted text: {temp}")
 
         return temp
